dbb898410cd31c3a6132384d5835f273b331a591
Martin <bugao1991@gmail.com>
2022-06-26 21:41:56 -0300

    mejora en alerta

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,24 +3,53 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
+
+  void displayDialog(BuildContext context){
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          elevation: 5,
+          title: const Text('Titulo'),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('Este es el contenido del alerta'),
+              SizedBox(height: 10),
+              FlutterLogo(size: 100,)
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancelar')
+            )
+          ],
+
+        );
+      }
+    );
+  }
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
          child: ElevatedButton(
-          child: const Text('Mostrar Alerta', style: TextStyle(fontSize:  20)),
-          onPressed: (){
-
-          },
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Text('Mostrar Alerta', style: TextStyle(fontSize: 16)),
+          ),
+          onPressed: () => displayDialog(context)
          )
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.close),
-        onPressed: (){
-          Navigator.pop(context);
+        onPressed: () => Navigator.pop(context)
 
-        }
+        
         ),
     );
   }
