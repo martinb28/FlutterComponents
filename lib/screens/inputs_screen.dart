7322de408ed1775efddd7e1f18330e3b070d23ae
Martin <bugao1991@gmail.com>
2022-07-17 18:47:18 -0300

    mejora del input_screen

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -16,13 +16,35 @@ class InputsScreen extends StatelessWidget {
           child: Column(
             children: [
               TextFormField(
-                autofocus: true,
-                initialValue: 'Ingresa tu nombre',
+                autofocus: false,
+                initialValue: '',
                 textCapitalization: TextCapitalization.words,
                 onChanged: (value) {
                   print('value $value');
                 },
-
+                validator: (value){
+                  if (value == null) return 'Este campo es requerido';
+                  return value.length < 3 ? 'Minimo de 3 letras': null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  hintText: 'Nombre del usuario',
+                  labelText: 'Nombre',
+                  helperText: 'Solo letras',                  
+                  suffixIcon: Icon(Icons.group_outlined),
+                  icon: Icon(Icons.assignment_ind_outlined),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.green
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                      )
+                    )
+                ),
               )
             ],
           ),
