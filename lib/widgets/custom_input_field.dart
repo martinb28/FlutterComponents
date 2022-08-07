af922ac62b33933606951346b9ad29295f461e35
Martin <bugao1991@gmail.com>
2022-08-07 20:57:48 -0300

    se finaliza form input

--- lib/widgets/custom_input_field.dart
+++ lib/widgets/custom_input_field.dart
@@ -7,6 +7,11 @@ class CustomInputField extends StatelessWidget {
   final String? helperText;
   final IconData? icon;
   final IconData? suffixIcon;
+  final TextInputType? keyboardType;
+  final bool isPassword;
+
+  final String formProperty;
+  final Map<String, String> formValues;
 
 
   const CustomInputField({
@@ -16,17 +21,22 @@ class CustomInputField extends StatelessWidget {
     this.helperText,
     this.icon,
     this.suffixIcon,
+    this.keyboardType,
+    this.isPassword = false,
+    required this.formProperty,
+    required this.formValues,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     return TextFormField(
       autofocus: false,
       initialValue: '',
       textCapitalization: TextCapitalization.words,
-      onChanged: (value) {
-        print('value: $value');
-      },
+      keyboardType:  keyboardType,
+      obscureText: isPassword,
+      onChanged: (value) => formValues[formProperty] = value,
       validator: (value){
         if (value == null) return 'Este campo es requerido';
         return value.length < 3 ? 'Minimo de 3 letras': null;
@@ -38,7 +48,7 @@ class CustomInputField extends StatelessWidget {
         helperText: helperText,
         suffixIcon: suffixIcon == null ? null : Icon(suffixIcon),
         icon: icon == null ? null : Icon(icon),
-      ),
+      ),      
     );
   }
 }
